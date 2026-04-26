f8ec854394504dd7ac5a1d9c6dbbbe657fb87e3c
NikaL25 <77246013+NikaL25@users.noreply.github.com>
2026-04-26 21:01:30 +0400

    Update axios baseURL to production endpoint

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:3002/api"
+    // baseURL: "http://localhost:3002/api"
+     baseURL: "https://mern-blog-app-0lw2.onrender.com/api"
 })
 
 instance.interceptors.request.use(config =>{
